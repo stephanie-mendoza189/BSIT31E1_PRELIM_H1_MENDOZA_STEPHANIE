cff014f72f61fad2d4ed5e168ff56a8e66d00f3d
stephanie-mendoza189 <tepteptep231@gmail.com>
2026-06-16 20:15:43 +0800

    TRY

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine(" ===== STUDENT SYSTEM ===== ");
 
 
-//testing only for git purpose
